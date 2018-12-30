8f9a4fa4515783e98091121dc214712d2e716f14
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2018-12-30 00:06:42 -0200

    .

--- bot.R
+++ bot.R
@@ -6,14 +6,16 @@ library(tibble)
 library(stringr)
 set.seed(1234)
 
+## não esqueça de mudar todas as chaves para o do seu bot, esse é do Jorgetron
+
 consumerKey = 'jIhYluFluZvG9eg7rw4IWr65p'
 consumerSecret = 'nAMRBixi3HvM01nHcyLRC0wDtubi9flVvDEc9RQW58kOQZz7Ru'
 accessToken = '1079161008986427393-mfKbK0DYeC7EFIlC4oOSNQj38KPzRw'
 accessTokenSecret = 'TFO4njpN9u8hGWnCjQUKvIRNovDwf9aP1npaNfelp3u2O'
 
-key = "7a67715b6d0dd6edf7556cc0d176e4ac"
+key = "7a67715b6d0dd6edf7556cc0d176e4ac" # chave do Vagalume API
 
-#Connect to twitter
+#entre no twitter
 setup_twitter_oauth(consumerKey, 
                     consumerSecret,
                     accessToken,
